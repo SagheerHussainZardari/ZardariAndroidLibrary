1542be39781fbfac330beca067802f25ca0cf246
MrZardari <sagheerhzardari@gmail.com>
2019-11-24 21:35:20 +0500

    Added Some More Functions For Password and Toast

--- zardarilibrary/src/main/java/com/sagheer/zardarilibrary/Form.kt
+++ zardarilibrary/src/main/java/com/sagheer/zardarilibrary/Form.kt
@@ -1,7 +1,33 @@
 package com.sagheer.zardarilibrary
 
+import android.app.Activity
 import android.util.Patterns
+import android.widget.Toast
 
 fun isEmailValid(email: String): Boolean{
     return Patterns.EMAIL_ADDRESS.matcher(email).matches()
+}
+
+fun isPassLength(pass: String, minLength: Int): Boolean {
+    return pass.length >= minLength
+}
+
+fun isEmailGmail(pass: String): Boolean {
+    return pass.toLowerCase().endsWith("gmail.com")
+}
+
+fun isPassAlphaNumeric(pass: String): Boolean {
+    return pass.contains(Regex("[a-z]")) && pass.contains(Regex("[0-9]"))
+}
+
+fun isPassContainingSmallCapitalAndNumber(pass: String): Boolean {
+    return pass.contains(Regex("[a-z]")) && pass.contains(Regex("[A-Z]")) && pass.contains(Regex("[0-9]"))
+}
+
+fun shortToast(message: String) {
+    Toast.makeText(Activity(), message, Toast.LENGTH_SHORT).show()
+}
+
+fun longToast(message: String) {
+    Toast.makeText(Activity(), message, Toast.LENGTH_LONG).show()
 }
